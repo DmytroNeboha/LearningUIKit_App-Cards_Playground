8dab2acc7d5b6779f94368703fb1d5c4c71dbfd6
DMYTRO <dmytro.neboha@proton.me>
2022-12-06 21:09:01 +0200

    16.7 Свойство bounds

--- ViewExperiments.playground/Contents.swift
+++ ViewExperiments.playground/Contents.swift
@@ -16,6 +16,11 @@ class MyViewController : UIViewController {
         let whiteView = getWhiteView()
         let pinkView = getPinkView()
         
+        
+        // поворот красного представления
+        redView.transform = CGAffineTransform(rotationAngle: .pi/3)
+        
+        
         set(view: greenView, toCenterOfView: redView)
         // позиционируем белое представление с помощю свойства center
         whiteView.center = greenView.center
@@ -31,65 +36,9 @@ class MyViewController : UIViewController {
     
     // создание представления розового цвета
     private func getPinkView() -> UIView {
-        let viewFrame = CGRect(x: 50, y: 330, width: 100, height: 100)
+        let viewFrame = CGRect(x: 50, y: 350, width: 100, height: 100)
         let view = UIView(frame: viewFrame)
         view.backgroundColor = .systemPink
-        
-        // Толщина границы
-        view.layer.borderWidth = 5
-        // Цвет границ
-        view.layer.borderColor = UIColor.yellow.cgColor
-        // Скругление углов
-        view.layer.cornerRadius = 10
-        // Видимость тени
-        view.layer.shadowOpacity = 0.95
-        // Радиус размытия тени
-        view.layer.shadowRadius = 20
-        // Смещение тени
-        view.layer.shadowOffset = CGSize(width: 10, height: 20)
-        // Цвет тени
-        view.layer.shadowColor = UIColor.white.cgColor
-        //  Прозрачность слоя
-        view.layer.opacity = 0.7
-        // Изменение фона
-        view.layer.backgroundColor = UIColor.black.cgColor
-        view.layer.backgroundColor = UIColor.systemPink.cgColor
-        
-        
-        // создание дочернего слоя
-        let layer = CALayer()
-        // изменение фонового цвета
-        layer.backgroundColor = UIColor.black.cgColor
-        // изменение размеров и положения
-        layer.frame = CGRect(x: 10, y: 10, width: 20, height: 20)
-        // изменение радиуса скругления углов
-        layer.cornerRadius = 10
-        // добавление в иерархию слоев
-        view.layer.addSublayer(layer)
-        
-        
-        // вывод на консоль размеров представления
-        print(view.frame)
-        // поворот представления
-//        view.transform = CGAffineTransform(rotationAngle: .pi/4)
-        // вывод на консоль размеров представления
-        print(view.frame)
-        
-        
-        // Растяжение
-//        view.transform = CGAffineTransform(scaleX: 1.5, y: 0.7)
-        
-        // Смещение
-        view.transform = CGAffineTransform(translationX: 100, y: 5)
-        
-        // Множественное преобразования
-        view.transform = CGAffineTransform(rotationAngle: .pi/3).scaledBy(x: 2, y: 0.8).translatedBy(x: 50, y: 50)
-        
-        // Свойтсво identity - применяют в анимации когда нужно отменить действия
-        view.transform = CGAffineTransform.identity
-        
-        // Метод inverted()
-        view.transform = CGAffineTransform(rotationAngle: .pi/4).inverted()
         
         
         return view
@@ -133,18 +82,21 @@ class MyViewController : UIViewController {
     
     
     private func set(view moveView: UIView, toCenterOfView baseView: UIView) {
-        // размеры вложеного представления
-        let moveViewWidth = moveView.frame.width
-        let moveViewHeight = moveView.frame.height
+        // свойсвта minX, minY, midX, midY, maxX,maxY
+        moveView.center = CGPoint(x: baseView.bounds.midX, y: baseView.bounds.midY)
         
-        // размеры родительского представления
-        let baseViewWidth = baseView.frame.width
-        let baseViewHeight = baseView.frame.height
-        
-        // вычисление и изменение координат
-        let newXCoordinate = (baseViewWidth - moveViewWidth) / 2
-        let newYCoordinate = (baseViewHeight - moveViewHeight) / 2
-        moveView.frame.origin = CGPoint(x: newXCoordinate, y: newYCoordinate)
+//        // размеры вложеного представления
+//        let moveViewWidth = moveView.frame.width
+//        let moveViewHeight = moveView.frame.height
+//
+//        // размеры родительского представления
+//        let baseViewWidth = baseView.bounds.width
+//        let baseViewHeight = baseView.bounds.height
+//
+//        // вычисление и изменение координат
+//        let newXCoordinate = (baseViewWidth - moveViewWidth) / 2
+//        let newYCoordinate = (baseViewHeight - moveViewHeight) / 2
+//        moveView.frame.origin = CGPoint(x: newXCoordinate, y: newYCoordinate)
     }
 }
 // Present the view controller in the Live View window
